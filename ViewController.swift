fc328cac10f4e31c669131a5d27cff9ba97e60db
Steve <steve@bisendesign.com>
2014-10-18 15:54:06 +0800

    Basic Function

--- ViewController.swift
+++ ViewController.swift
@@ -11,9 +11,11 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     
+    @IBOutlet weak var mailButton: UIButton!
     
     
     
@@ -33,8 +35,17 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        
+        mailButton.setTitle("Mail sent", forState: UIControlState.Normal)
     }
 }
 
